cc041df70c3f2bb5c1e38c0c4b5486efab5f0b0c
Russell Brabers <brabers.russell@gmail.com>
2020-12-18 16:30:44 -0500

    Platform update
    - Platform shrinks on contact
    - Player can jump off platform

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -282,6 +282,11 @@ public class PlayerBehaviour : MonoBehaviour
             other.gameObject.GetComponent<MovingPlatformController>().isActive = true;
             transform.SetParent(other.gameObject.transform);
         }
+
+        if(other.gameObject.CompareTag("Shrinking Platform"))
+        {
+            other.gameObject.GetComponent<ShrinkPlatformController>().TriggerShrink();
+        }
     }
 
     private void OnCollisionExit2D(Collision2D other)
---
+++ Assets/_Scripts/ShrinkPlatformController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkPlatformController : MonoBehaviour
+{
+    private Animator anim;
+    public bool canGrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        canGrow = true;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TriggerGrow();
+        }
+    }
+
+    public void TriggerShrink()
+    {
+        if (canGrow)
+        {
+            anim.SetTrigger("isShrinking");
+            canGrow = false;
+        }
+    }
+
+    public void TriggerIdleSmall()
+    {
+        anim.SetTrigger("isIdleSmall");
+    }
+
+    public void TriggerGrow()
+    {
+        StartCoroutine(GrowRoutine());
+    }
+    IEnumerator GrowRoutine()
+    {
+        yield return new WaitForSeconds(1);
+        anim.SetTrigger("isGrowing");
+    }
+
+    public void TriggerIdle()
+    {
+        anim.SetTrigger("isIdle");
+        canGrow = true;
+    }
+}
